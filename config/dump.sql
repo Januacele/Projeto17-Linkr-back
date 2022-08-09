dc2a176d64578d21432b72280fa04bda66bf81ed
Januacele <januacelevieira@hotmail.com>
2022-08-09 20:04:06 -0300

    creat tables database

--- config/dump.sql
+++ config/dump.sql
@@ -1,7 +1,82 @@
-CREATE TABLE "users" (
-"id" SERIAL NOT NULL PRIMARY KEY,
-"name" varchar(120) NOT NULL,
-"email" varchar(100) UNIQUE NOT NULL,
-"password" varchar(255) NOT NULL,
-"createdAt" TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT NOW()    
+
+CREATE TABLE "users"
+(
+    "id" SERIAL NOT NULL PRIMARY KEY,
+    "username" text UNIQUE NOT NULL,
+    "email" text UNIQUE NOT NULL,
+    "password" text NOT NULL,
+    "profile_image" text NOT NULL,
+    "created_at" TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE "sessions"
+(
+    "id" SERIAL NOT NULL PRIMARY KEY,
+    "user_id" INTEGER NOT NULL REFERENCES "users"("id"),
+    "token" text UNIQUE NOT NULL,
+    "created_at" TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE "posts"
+(
+    "id" SERIAL NOT NULL PRIMARY KEY,
+    "user_id" INTEGER NOT NULL REFERENCES "users"("id"),
+    "message" TEXT,
+    "shared_url" TEXT NOT NULL,
+    "created_at" TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT NOW(),
+);
+
+CREATE TABLE "postsDeleted"
+(
+    "id" SERIAL NOT NULL PRIMARY KEY,
+    "user_id" INTEGER NOT NULL REFERENCES "users"("id"),
+    "post_id" INTEGER NOT NULL REFERENCES "posts"("id"),
+    "deleted_at" TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE "hashtags"
+(
+    "id" SERIAL NOT NULL PRIMARY KEY,
+    "name" TEXT UNIQUE NOT NULL,
+    "created_at" TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE "PostsHashtags"
+(
+    "id" SERIAL NOT NULL PRIMARY KEY,
+    "post_id" INTEGER NOT NULL REFERENCES "posts"("id"),
+    "created_at" TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE "reposts"
+(
+    "id" SERIAL NOT NULL PRIMARY KEY,
+    "post_id" INTEGER NOT NULL REFERENCES "posts"("id"),
+    "user_id" INTEGER NOT NULL REFERENCES "users"("id"),
+    "created_at" TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE "comments"
+(
+    "id" SERIAL NOT NULL PRIMARY KEY,
+    "post_id" INTEGER NOT NULL REFERENCES "posts"("id"),
+    "user_id" INTEGER NOT NULL REFERENCES "users"("id"),
+    "message" varchar(200),
+    "created_at" TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE "likes"
+(
+    "id" SERIAL NOT NULL PRIMARY KEY,
+    "user_id" INTEGER NOT NULL REFERENCES "users"("id"),
+    "post_id" INTEGER NOT NULL REFERENCES "posts"("id"),
+    "created_at" TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT NOW() 
+);
+
+CREATE TABLE "follows"
+(
+    "id" SERIAL NOT NULL PRIMARY KEY,
+    "followed_id" INTEGER NOT NULL REFERENCES "users"("id"),
+    "follower_id" INTEGER NOT NULL REFERENCES "users"("id"),
+    "created_at" TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT NOW() 
 );
