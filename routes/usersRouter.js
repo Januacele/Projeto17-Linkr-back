0964264aefd5be09c4f0412bb49bd33488f59f31
Januacele <januacelevieira@hotmail.com>
2022-08-12 13:19:06 -0300

    updates

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -2,11 +2,11 @@ import { Router } from 'express';
 import { validateToken } from '../middlewares/validateToken.js';
 import { schemaValidator } from '../middlewares/schemaValidator.js';
 import { getUsersByNameSchema } from '../schemas/userSchemas.js';
-import { getUsersByNameFollowersFirst} from '../controllers/usersController.js';
+import { getUsersByNameFollowersFirst, getUser} from '../controllers/usersController.js';
 
 const usersRouter = Router ();
 
 usersRouter.post('/users', validateToken, schemaValidator(getUsersByNameSchema), getUsersByNameFollowersFirst);
-
+usersRouter.get('/user', getUser);
 
 export default usersRouter;
