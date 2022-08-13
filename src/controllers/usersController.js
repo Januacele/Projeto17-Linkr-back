8ec06a31ea437d4079e203d11775149d85abb57d
Januacele <januacelevieira@hotmail.com>
2022-08-12 23:44:35 -0300

    Adjustment at usersController function

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,15 +1,24 @@
 import usersRepository from '../repositories/usersRepository.js';
+import db from '../config/db.js';
 
-
-export async function getUsersByNameFollowersFirst(req, res){
-    const { username } = req.query
-    const { userId } = res.locals
+export const getUsersByNameFollowersFirst = async (req, res) => {
     try {
-        const result = await usersRepository.getUserByName(username, userId);
-        return res.status(200).send(result.rows);
-
-    } catch (error) {
-        return res.status(500).send(error);
+        const {searchString} = req.body
+        const result = await db.query(
+            `SELECT * FROM (
+                (
+                    SELECT users.id, users.username, users.profile_image, (users.id IN (
+                        SELECT follows.followed_id FROM follows
+                        WHERE follows.follower_id = $1
+                    )) AS follower FROM users
+                )
+            ) AS result WHERE result.username LIKE $2
+            ORDER BY follower DESC;`,
+            [res.locals.user_id, searchString + '%']
+        )
+        return res.status(200).send(result)
+    } catch(error) { 
+        return res.status(500).send(error) 
     }
 }
 
--- src/repositories/usersRepository.js
+++ src/repositories/usersRepository.js
@@ -5,21 +5,6 @@ async function getUserById(userId) {
     return db.query(`SELECT * FROM users WHERE id = $1 `, [userId]);
   }
 
-async function getUserByName(){
-  const { searchString } = req.body;
-  return db.query(`
-    SELECT * FROM (
-      (
-        SELECT users.id, users.username, users.profile_image, (users.id IN (
-          SELECT follows.followed_id FROM follows
-          WHERE follows.follower_id = $1
-        )) AS follower FROM users
-      )
-    ) AS result WHERE result.username LIKE $2
-    ORDER BY follower DESC`, [res.locals.user_id, searchString + '%']
-    );
-}
-
 async function getUser(){
   const {authorization} = req.headers
   const token = authorization?.replace("Bearer", "").trim()
@@ -33,7 +18,6 @@ async function getUser(){
 
 const usersRepository = {
   getUserById,
-  getUserByName,
   getUser
 }
 
