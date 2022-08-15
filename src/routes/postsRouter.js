8f16b6d95df5dec67be9dd8a1a90610f70b43c54
Januacele <januacelevieira@hotmail.com>
2022-08-15 16:08:40 -0300

    get posts

--- src/routes/postsRouter.js
+++ src/routes/postsRouter.js
@@ -1,5 +1,5 @@
 import { Router } from "express"
-import { savePost, editPostController, deletePostController } from "../controllers/postsController.js"
+import { savePost, editPostController, deletePostController, getPosts } from "../controllers/postsController.js"
 import { validateSchema } from "../middlewares/schemaValidator.js"
 import { tokenValidation } from "../middlewares/tokenValidation.js"
 import { editPostValidation, deletePostValidation } from "../middlewares/validatePost.js"
@@ -8,6 +8,7 @@ import { postSchema, editPostSchema } from "../schemas/postSchema.js"
 const postsRouter = Router()
 
 postsRouter.post("/posts", tokenValidation, validateSchema(postSchema), savePost)
+postsRouter.get("/posts", tokenValidation, getPosts);
 postsRouter.put('/editpost', editPostValidation, validateSchema(editPostSchema), editPostController)
 postsRouter.delete('/deletepost/:id', deletePostValidation, deletePostController)
 
