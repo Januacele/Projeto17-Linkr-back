dd98848b06aadfb1814bf15b073027d02e9f3d86
SADoug <douglastec2@gmail.com>
2022-08-18 19:18:39 -0300

    Fix: hashtags posts

--- src/routes/postsRouter.js
+++ src/routes/postsRouter.js
@@ -4,13 +4,15 @@ import { validateSchema } from "../middlewares/schemaValidator.js"
 import { tokenValidation } from "../middlewares/tokenValidation.js"
 import { deletePostValidation } from "../middlewares/validatePost.js"
 import { postSchema } from "../schemas/postSchema.js"
+import { userHashtagValidation } from '../middlewares/validatePost.js';
+import {getTimelineController} from '../controllers/timelineController.js';
 
 const postsRouter = Router()
 
 postsRouter.post("/posts", tokenValidation, validateSchema(postSchema), savePost)
 postsRouter.get("/posts", tokenValidation, getPosts);
 postsRouter.get("/posts/new/:id", tokenValidation, getNewPosts);
-postsRouter.get("/posthashtags/:name", getPostsByHashtag);
+postsRouter.get("/posthashtags/:name",tokenValidation, userHashtagValidation, getTimelineController);
 postsRouter.put('/editpost/:id', tokenValidation, editPost)
 postsRouter.delete('/deletepost/:id', deletePostValidation, deletePostController)
 
